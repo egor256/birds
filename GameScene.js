c5650321e8988028897338d795c19d0383918bcf
Andrea Cavalheiro <andrea_cavalheiro@hotmail.com>
2019-07-23 21:01:39 -0400

    Adding objects
    Adding objects

--- GameScene.js
+++ GameScene.js
@@ -1,6 +1,7 @@
 class GameScene {
 
     static init(stage) {
+        var i,j,k;
         GameScene.stage = stage;
         GameScene.stage.removeAllChildren();
         GameScene.stage.update();
@@ -26,23 +27,43 @@ class GameScene {
         GameScene.stage.addChild(GameScene.nourishmentBoundingRect);
         
         // TODO: add more elements for the level here...
-        new Tree(GameScene.stage,  850, 320);
-        new Tree(GameScene.stage, 1400, 320);
-        new Tree(GameScene.stage, 1600, 320);
-        new Tree(GameScene.stage, 2000, 320);
-        new Tree(GameScene.stage, 2500, 320);
-        new FoodItem(GameScene.stage, "lemon", 800, 100);
-        new FoodItem(GameScene.stage, "banana", 200, 250);
-        new FoodItem(GameScene.stage, "apple", 1600, 100);        
-        new RedBird(GameScene.stage, 1000, 330);
-        new RedBird(GameScene.stage, 1400, 320);
-        new RedBird(GameScene.stage, 1700, 380);
-        new FoodItem(GameScene.stage, "lemon", 1800, 100);
-        new FoodItem(GameScene.stage, "banana", 2000, 250);
-        new FoodItem(GameScene.stage, "apple", 2500, 300);
-        new RedBird(GameScene.stage, 1800, 130);
-        new RedBird(GameScene.stage, 2300, 150);
-        new RedBird(GameScene.stage, 2600, 140);
+        //new Tree(GameScene.stage,  850, 320);
+        //new Tree(GameScene.stage, 1400, 320);
+        //new Tree(GameScene.stage, 1600, 320);
+        //new Tree(GameScene.stage, 2000, 320);
+        //new Tree(GameScene.stage, 2500, 320);
+        //new FoodItem(GameScene.stage, "lemon", 800, 100);
+        //new FoodItem(GameScene.stage, "banana", 200, 250);
+        //new FoodItem(GameScene.stage, "apple", 1600, 100);        
+        //new RedBird(GameScene.stage, 1000, 330);
+        //new RedBird(GameScene.stage, 1400, 320);
+        //new RedBird(GameScene.stage, 1700, 380);
+        //new FoodItem(GameScene.stage, "lemon", 1800, 100);
+        //new FoodItem(GameScene.stage, "banana", 2000, 250);
+        //new FoodItem(GameScene.stage, "apple", 2500, 300);
+        //new RedBird(GameScene.stage, 1800, 130);
+        //new RedBird(GameScene.stage, 2300, 150);
+        //new RedBird(GameScene.stage, 2600, 140);
+
+        for (i = 100; i < 10000; i=i+500) 
+        {   
+            new Tree(GameScene.stage, i, 320);          
+        }
+
+        for (j = 100; j < 10000; j=j+1000) 
+        { 
+            new FoodItem(GameScene.stage, "lemon",  j,     150);
+            new FoodItem(GameScene.stage, "banana", j+400, 280);
+            new FoodItem(GameScene.stage, "apple",  j+800, 220);  
+        }
+
+        for (k = 1000; k < 30000; k=k+2000) 
+        { 
+            new RedBird(GameScene.stage, k,      150);
+            new RedBird(GameScene.stage, k+500,  250);
+            new RedBird(GameScene.stage, k+1200, 380);
+            new RedBird(GameScene.stage, k+1500, 160);
+        }
     }
 
     // val - [0..100], bar size - 200
